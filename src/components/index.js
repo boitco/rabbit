36e066f71b97eab476466c7c8ed7b9b8778af0d5
boitco <2604582901@qq.com>
2025-06-08 00:37:21 +0800

    详情页-全局组件插件化

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+// 全局化注册
+import ImageView from './ImageView/index.vue'
+import Sku from './XtxSku/index.vue'
+export const componentPlugin = {
+  install(app) {
+    app.component('XtxImageView', ImageView)
+    app.component('XtxSku', Sku)
+
+  }
+}
