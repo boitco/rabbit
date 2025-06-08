a8806d87a0fac1b195d1606fdaef7165faac09a1
boitco <2604582901@qq.com>
2025-06-08 21:56:04 +0800

    登录-表单校验-统一校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from "vue";
+import { ElMessage } from "element-plus";
+import "element-plus/theme-chalk/el-message.css";
 // 表单校验功能（账号名+密码）
 // 1.准备表单对象
 const form = ref({
@@ -30,6 +32,19 @@ const rules = {
     },
   ],
 };
+
+// 3.或者form实例做统一校验
+const formRef = ref(null);
+const doLogin = () => {
+  formRef.value.validate((valid) => {
+    // valid:所有表单都通过校验才为true
+    // console.log(valid);
+    // 以valid作为判断条件，如果通过校验才执行登录逻辑
+    if (valid) {
+      // TODO LOGIN
+    }
+  });
+};
 </script>
 
 
@@ -55,6 +70,7 @@ const rules = {
         <div class="account-box">
           <div class="form">
             <el-form
+              ref="formRef"
               :model="form"
               :rules="rules"
               label-position="right"
@@ -72,7 +88,9 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin"
+                >点击登录</el-button
+              >
             </el-form>
           </div>
         </div>
